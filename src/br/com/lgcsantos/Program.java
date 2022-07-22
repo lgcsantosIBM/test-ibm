5c1821ca47d9c8953c5697f6717100ca12e9605e
lgcsantos <lucas.lgcsantos@ibm.com>
2022-07-22 11:42:04 -0300

    test repository

---
+++ src/br/com/lgcsantos/Program.java
@@ -0,0 +1,8 @@
+package br.com.lgcsantos;
+
+public class Program {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Word");
+    }
+}
---
+++ src/br/com/lgcsantos/User.java
@@ -0,0 +1,4 @@
+package br.com.lgcsantos;
+
+public class User {
+}
